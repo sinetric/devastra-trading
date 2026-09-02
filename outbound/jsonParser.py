1b5c94eeed83cb2711e488be40f01d1ec300c401
memezine <zerogaming809@gmail.com>
2026-09-03 09:28:11 +1000

    JSON parser & synthesizer for the outbound module.

---
+++ outbound/jsonParser.py
@@ -0,0 +1,11 @@
+import json
+
+def create_json_file(file_path, data): # create json file and write data to it
+    with open(file_path, "w") as json_file:
+        json.dump(data.text, json_file)
+    return file_path
+
+def parse_json_file(file_path): # parse json file and return data
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+    return data
