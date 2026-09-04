7c4ca9716bd6513dfe42d918904e32c1e385f31b
memezine <zerogaming809@gmail.com>
2026-09-04 17:28:36 +1000

    Temporarily made main.py do nothing

--- outbound/market-data/stocks.py
+++ outbound/market-data/stocks.py
@@ -1,13 +1,16 @@
 import requests
 import json
+from utils.jsonParser import create_json_file, parse_json_file
 from header import get_header
 
-class TopMarketMovers:
-    def __init__(self):
-        self.url = "https://data.alpaca.markets/v1beta1/screener/stocks/movers?top=10"
-        self.headers = get_header()
+def fetch_top_market_movers(dump_to_json, json_file_path):
+    url = "https://data.alpaca.markets/v1beta1/screener/stocks/movers?top=10"
+    headers = get_header()
 
-    def fetch(self):
-        response = requests.get(self.url, headers=self.headers)
-        return response.json()  # Return the JSON response directly
+    response = requests.get(url, headers=headers)
+
+    if (dump_to_json and json_file_path):
+        create_json_file(json_file_path, response)  # Create a JSON file with the response data
+
+    return response.json()  # Return the JSON response directly
 
