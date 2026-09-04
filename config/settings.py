6df2dd816d6f25d7dacea1a30075bdcb5588290f
memezine <zerogaming809@gmail.com>
2026-09-04 23:30:34 +1000

    Updated interval

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class Settings(BaseModel):
     BASE_URL: str = "https://paper-api.alpaca.markets" # paper trading base URL
     ENABLE_DRY_RUNNING: bool = True # enable dry running for testing purposes (no trades will be executed)
-    SCAN_INTERVAL_SECONDS: int = 5 # how often to re-scan the watchlist while market is open
+    SCAN_INTERVAL_SECONDS: int = 10 # how often to re-scan the watchlist while market is open
     CLOSED_MARKET_SLEEP_SECONDS: int = 300 # check less frequently while the market's closed
     RUN_DESPITE_MARKET_CLOSED: bool = True # if True, the bot will run even when the market is closed (useful for testing)
 
