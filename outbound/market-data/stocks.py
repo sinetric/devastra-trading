7d3292713c1d545d67556f2d0f20de80f8a20105
memezine <zerogaming809@gmail.com>
2026-09-04 16:19:04 +1000

    Renamed method for conciseness

--- outbound/market-data/stocks.py
+++ outbound/market-data/stocks.py
@@ -7,6 +7,7 @@ class TopMarketMovers:
         self.url = "https://data.alpaca.markets/v1beta1/screener/stocks/movers?top=10"
         self.headers = get_header()
 
-    def fetch_top_movers(self):
+    def fetch(self):
         response = requests.get(self.url, headers=self.headers)
         return response.json()  # Return the JSON response directly
+
