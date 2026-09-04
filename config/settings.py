80b79c3be2a6ffffcb393c9fe4bf1e5fa060417e
memezine <zerogaming809@gmail.com>
2026-09-04 19:23:19 +1000

    Fixed silly mistake and returned to BaseModel

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
 
 class Settings(BaseModel):
-    BASE_URL: str
+    BASE_URL: str = "https://paper-api.alpaca.markets" # paper trading base URL
+    ENABLE_DRY_RUNNING: bool = True # enable dry running for testing purposes (no trades will be executed)
 
 def get_settings():
-    return Settings(
-        BASE_URL="https://paper-api.alpaca.markets", # paper trading base URL
-    )
+    return Settings()
